0249176554c936c191e55fe7d96699d833345b50
Javier Aviles <avileslopez.javier@gmail.com>
2019-10-27 22:52:46 +0100

    Adding Start Controller Test

---
+++ src/main/java/controllers/StartController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import models.Game;
+import models.State;
+
+public class StartController {
+
+	public StartController(Game game, State state) {
+	}
+
+	public void play() {
+	}
+
+}
