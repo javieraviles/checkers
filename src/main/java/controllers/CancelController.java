3fb422e4072159baa742dbbc3804c1e86ab3b642
Javier Aviles <avileslopez.javier@gmail.com>
2019-10-27 23:02:05 +0100

    Cancel and Resume controllers  tests

---
+++ src/main/java/controllers/CancelController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import models.Game;
+import models.State;
+
+public class CancelController {
+
+	public CancelController(Game game, State state) {
+	}
+
+	public void stopGame() {
+	}
+
+}
---
+++ src/main/java/controllers/ResumeController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import models.Game;
+import models.State;
+
+public class ResumeController {
+
+	public ResumeController(Game game, State state) {
+	}
+
+	public void resume(boolean playAgain) {
+	}
+
+}
