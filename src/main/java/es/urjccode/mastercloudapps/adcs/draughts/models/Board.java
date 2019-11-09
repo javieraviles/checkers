611f665822a0890647bef6d4acb111d7e5460d0c
Javier Aviles <avileslopez.javier@gmail.com>
2019-11-09 19:55:47 +0100

    Using StringBuilder in board

--- src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Board.java
+++ src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Board.java
@@ -5,95 +5,95 @@ import java.util.List;
 
 class Board {
 
-    private static final int DIMENSION = 8;
+	private static final int DIMENSION = 8;
 
-    private Square[][] squares;
+	private Square[][] squares;
 
-    Board() {
-        this.squares = new Square[this.getDimension()][this.getDimension()];
-        for (int i = 0; i < this.getDimension(); i++) {
-            for (int j = 0; j < this.getDimension(); j++) {
-                this.squares[i][j] = new Square();
-            }
-        }
-    }
+	Board() {
+		this.squares = new Square[this.getDimension()][this.getDimension()];
+		for (int i = 0; i < this.getDimension(); i++) {
+			for (int j = 0; j < this.getDimension(); j++) {
+				this.squares[i][j] = new Square();
+			}
+		}
+	}
 
-    private Square getSquare(Coordinate coordinate){
-        assert coordinate!=null && coordinate.isValid();
-        return this.squares[coordinate.getRow()][coordinate.getColumn()];
-    }
+	private Square getSquare(Coordinate coordinate) {
+		assert coordinate != null && coordinate.isValid();
+		return this.squares[coordinate.getRow()][coordinate.getColumn()];
+	}
 
-    void put(Coordinate coordinate, Piece piece){
-        assert piece != null;
-        this.getSquare(coordinate).put(piece);
-    }
+	void put(Coordinate coordinate, Piece piece) {
+		assert piece != null;
+		this.getSquare(coordinate).put(piece);
+	}
 
-    Piece remove(Coordinate coordinate) {
-        assert this.getPiece(coordinate) != null;
-        return this.getSquare(coordinate).remove();
-    }
+	Piece remove(Coordinate coordinate) {
+		assert this.getPiece(coordinate) != null;
+		return this.getSquare(coordinate).remove();
+	}
 
-    void move(Coordinate origin, Coordinate target) {
-        this.put(target, this.remove(origin));
-    }
+	void move(Coordinate origin, Coordinate target) {
+		this.put(target, this.remove(origin));
+	}
 
-    Piece getPiece(Coordinate coordinate) {
-        return this.getSquare(coordinate).getPiece();
-    }
+	Piece getPiece(Coordinate coordinate) {
+		return this.getSquare(coordinate).getPiece();
+	}
 
-    boolean isEmpty(Coordinate coordinate) {
-        return this.getSquare(coordinate).isEmpty();
-    }
-    
-    Color getColor(Coordinate coordinate) {
-        return this.getSquare(coordinate).getColor();
-    }
+	boolean isEmpty(Coordinate coordinate) {
+		return this.getSquare(coordinate).isEmpty();
+	}
 
-    List<Piece> getPieces(Color color) {
-        List<Piece> pieces = new ArrayList<Piece>();
-        for (int i = 0; i < this.getDimension(); i++) {
-            for (int j = 0; j < this.getDimension(); j++) {
-                pieces.add(this.squares[i][j].getPiece());
-            }
-        }
+	Color getColor(Coordinate coordinate) {
+		return this.getSquare(coordinate).getColor();
+	}
+
+	List<Piece> getPieces(Color color) {
+		List<Piece> pieces = new ArrayList<Piece>();
+		for (int i = 0; i < this.getDimension(); i++) {
+			for (int j = 0; j < this.getDimension(); j++) {
+				pieces.add(this.getSquare(new Coordinate(i, j)).getPiece());
+			}
+		}
 		return pieces;
 	}
-    
-    int getDimension() {
+
+	int getDimension() {
 		return Board.DIMENSION;
 	}
 
-    @Override
-    public String toString() {
-        String string = "";
-        string += this.toStringHorizontalNumbers();
-        for (int i = 0; i < this.getDimension(); i++) {
-            string += this.toStringHorizontalPiecesWithNumbers(i);
-        }
-        string += this.toStringHorizontalNumbers();
-        return string;
-    }
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(this.toStringHorizontalNumbers());
+		for (int i = 0; i < this.getDimension(); i++) {
+			builder.append(this.toStringHorizontalPiecesWithNumbers(i));
+		}
+		builder.append(this.toStringHorizontalNumbers());
+		return builder.toString();
+	}
 
-    private String toStringHorizontalNumbers(){
-        String string = " ";
-        for (int j = 0; j < Board.DIMENSION; j++) {
-            string += j;
-        }
-        return string + "\n";
-    }
+	private String toStringHorizontalNumbers() {
+		StringBuilder builder = new StringBuilder(" ");
+		for (int j = 0; j < Board.DIMENSION; j++) {
+			builder.append(j);
+		}
+		return builder.append("\n").toString();
+	}
 
-    private String toStringHorizontalPiecesWithNumbers(int row){
-        String string = "" + row;
-        for (int j = 0; j < this.getDimension(); j++) {
-            Piece piece = this.getPiece(new Coordinate(row, j));
-            if (piece == null) {
-                string += " ";
-            } else {
-                final String[] letters = {"b","n"};
-                string += letters[piece.getColor().ordinal()];
-            }
-        }
-        return string + row + "\n";
-    }
+	private String toStringHorizontalPiecesWithNumbers(int row) {
+		StringBuilder builder = new StringBuilder(row);
+		for (int j = 0; j < this.getDimension(); j++) {
+			Piece piece = this.getPiece(new Coordinate(row, j));
+			if (piece == null) {
+				builder.append(" ");
+			} else {
+				final String[] letters = { "b", "n" };
+				builder.append(letters[piece.getColor().ordinal()]);
+			}
+		}
+		return builder.append(row + "\n").toString();
+	}
 
 }
