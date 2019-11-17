c3c1ffa02451887ee05e6cfac170d1e0c3cb458f
Javier Aviles <avileslopez.javier@gmail.com>
2019-11-18 00:26:54 +0100

    Adding isblocked test

--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/PlayControllerTest.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/PlayControllerTest.java
@@ -2,11 +2,13 @@ package es.urjccode.mastercloudapps.adcs.draughts.controllers;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import es.urjccode.mastercloudapps.adcs.draughts.models.Coordinate;
 import es.urjccode.mastercloudapps.adcs.draughts.models.Game;
+import es.urjccode.mastercloudapps.adcs.draughts.models.GameBuilder;
 import es.urjccode.mastercloudapps.adcs.draughts.models.State;
 import es.urjccode.mastercloudapps.adcs.draughts.models.Color;
 
@@ -24,24 +26,25 @@ public class PlayControllerTest {
         assertNull(playController.getPiece(origin));
         assertEquals(playController.getColor(target), Color.WHITE);
     }
-
-    // public void data(){
-    //     Coordinate[][] coordinates = new Coordinate[][] {
-    //         { new Coordinate(0,0), new Coordinate(0,0) },
-    //         { new Coordinate(0,0), new Coordinate(0,0), new Coordinate(0,0), new Coordinate(0,0) },
-    //         { new Coordinate(0,0), new Coordinate(0,0) },
-    //     };
-
-        // peon mueve una
-        // peon come una
-        // peon come varias
-        // peon se convierte en dama
-        // dama mueve una
-        // dama mueve varias
-        // dama come una
-        // dama come varias, atras
-
-    //}
-
+    
+    @Test
+    public void givenPlayControllerWhenNoMoreMovementAvailableThenWinner() {
+        Coordinate origin = new Coordinate(2,5);
+        Coordinate target = new Coordinate(0,7);
+        State state = new State();
+        Game game = new GameBuilder()
+                .row("        ")
+                .row("      n ")
+                .row("     b  ")
+                .row("        ")
+                .row("        ")
+                .row("        ")
+                .row("        ")
+                .row("        ")
+                .build();
+        PlayController playController = new PlayController(game, state);
+        playController.move(origin, target);
+        assertTrue(playController.isBlocked());
+    }
 
 }
